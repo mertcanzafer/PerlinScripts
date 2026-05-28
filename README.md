06782cc920cc6c28ec1c7b29cb922941419c6c15
Mertcan Zafer <mertcanzafer06@gmail.com>
2026-05-28 12:47:06 +0300

    JsonBuilder.py, cmnd line argument

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Helper batch and Python scripts used to simplify development workflows for the P
 - **HardcodedJsonBuilder.py**
   - Deserializes hardcoded source JSON files, processes their contents, and serializes the result into destination JSON files.
   - Currently used for generating and updating texture registry data from mesh asset metadata.
+
+- **JsonBuilder.py**
+  - Deserializes source and destination JSON files provided through command-line arguments.
+  - Processes mesh texture metadata and appends generated texture entries into destination JSON registries.
---
+++ Scripts/JsonBuilder.py
@@ -0,0 +1,45 @@
+import json
+import sys
+
+def DeserializeJsonFile(fileName):
+    path = "files/" + fileName
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print("Error: The file was not found.")
+        return None
+    
+def SerializeJsonFile(fileName, data):
+    path = "files/" + fileName
+    try:
+        jsonStr = json.dumps(data,indent=4)
+        with open(path, 'w') as file:
+            file.write(jsonStr)
+            return 1
+    except FileNotFoundError:
+        print("Error: The file was not found.")
+        return None
+
+srcFile = sys.argv[1]
+dstFile = sys.argv[2]
+
+srcData = DeserializeJsonFile(srcFile)
+dstData = DeserializeJsonFile(dstFile)
+
+if(srcData is None):
+    print("Error occured")
+else:
+    print(json.dumps(srcData, indent=4))
+
+    for m in srcData["meshes"]:
+        for t in m["textures"]:
+            dstData["textures"].append({
+                "name": t
+            })
+
+res = SerializeJsonFile("textures.json",dstData)
+
+if(res is None):
+    print("Error occured")
